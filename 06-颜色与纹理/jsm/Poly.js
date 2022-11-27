c26906c1a35950317cd00316f8c8a5b916896d34
hanke_janson <1048458653@163.com>
2022-11-27 19:57:21 +0800

    webgl第四天

--- 06-颜色与纹理/jsm/Poly.js
+++ 06-颜色与纹理/jsm/Poly.js
@@ -23,47 +23,49 @@ maps 数据结构:{
 }
 */
 const defAttr = () => ({
-  gl:null,
-  type:'POINTS',
-  source:[],
-  sourceSize:0,
-  elementBytes:4,
+  gl: null,
+  type: "POINTS",
+  source: [],
+  sourceSize: 0,
+  elementBytes: 4,
   categorySize: 0,
   attributes: {},
   uniforms: {},
-  maps: {}
-})
-export default class Poly{
-  constructor(attr){
-    Object.assign(this,defAttr(),attr)
-    this.init()
+  maps: {},
+});
+export default class Poly {
+  constructor(attr) {
+    Object.assign(this, defAttr(), attr);
+    this.init();
   }
-  init(){
-    if (!this.gl) { return }
-    this.calculateSize()
+  init() {
+    if (!this.gl) {
+      return;
+    }
+    this.calculateSize();
     this.updateAttribute();
     this.updateUniform();
-    this.updateMaps()
+    this.updateMaps();
   }
   calculateSize() {
-    const {attributes, elementBytes,source } = this
-    let categorySize = 0
-    Object.values(attributes).forEach(ele => {
-      const { size, index } = ele
-      categorySize += size
-      ele.byteIndex=index*elementBytes
-    })
-    this.categorySize = categorySize
-    this.categoryBytes=categorySize*elementBytes
-    this.sourceSize = source.length / categorySize
+    const { attributes, elementBytes, source } = this;
+    let categorySize = 0;
+    Object.values(attributes).forEach((ele) => {
+      const { size, index } = ele;
+      categorySize += size;
+      ele.byteIndex = index * elementBytes;
+    });
+    this.categorySize = categorySize;
+    this.categoryBytes = categorySize * elementBytes;
+    this.sourceSize = source.length / categorySize;
   }
   updateAttribute() {
-    const { gl, attributes, categoryBytes, source } = this
+    const { gl, attributes, categoryBytes, source } = this;
     const sourceBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, sourceBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(source), gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(source), gl.STATIC_DRAW);
     for (let [key, { size, byteIndex }] of Object.entries(attributes)) {
-      const attr = gl.getAttribLocation(gl.program, key)
+      const attr = gl.getAttribLocation(gl.program, key);
       gl.vertexAttribPointer(
         attr,
         size,
@@ -71,24 +73,24 @@ export default class Poly{
         false,
         categoryBytes,
         byteIndex
-      )
-      gl.enableVertexAttribArray(attr)
+      );
+      gl.enableVertexAttribArray(attr);
     }
   }
   updateUniform() {
-    const {gl,uniforms}=this
+    const { gl, uniforms } = this;
     for (let [key, val] of Object.entries(uniforms)) {
-      const { type, value } = val
-      const u = gl.getUniformLocation(gl.program, key)
-      if (type.includes('Matrix')) {
-        gl[type](u,false,value)
+      const { type, value } = val;
+      const u = gl.getUniformLocation(gl.program, key);
+      if (type.includes("Matrix")) {
+        gl[type](u, false, value);
       } else {
-        gl[type](u,value)
+        gl[type](u, value);
       }
     }
   }
   updateMaps() {
-    const { gl, maps } = this
+    const { gl, maps } = this;
     Object.entries(maps).forEach(([key, val], ind) => {
       const {
         format = gl.RGB,
@@ -96,56 +98,35 @@ export default class Poly{
         wrapS,
         wrapT,
         magFilter,
-        minFilter
-      } = val
+        minFilter,
+      } = val;
 
-      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1)
-      gl.activeTexture(gl[`TEXTURE${ind}`])
-      const texture = gl.createTexture()
-      gl.bindTexture(gl.TEXTURE_2D, texture)
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+      gl.activeTexture(gl[`TEXTURE${ind}`]);
+      const texture = gl.createTexture();
+      gl.bindTexture(gl.TEXTURE_2D, texture);
 
-      gl.texImage2D(
-        gl.TEXTURE_2D,
-        0,
-        format,
-        format,
-        gl.UNSIGNED_BYTE,
-        image
-      )
+      gl.texImage2D(gl.TEXTURE_2D, 0, format, format, gl.UNSIGNED_BYTE, image);
 
-      wrapS&&gl.texParameteri(
-        gl.TEXTURE_2D,
-        gl.TEXTURE_WRAP_S,
-        wrapS
-      )
-      wrapT&&gl.texParameteri(
-        gl.TEXTURE_2D,
-        gl.TEXTURE_WRAP_T,
-        wrapT
-      )
+      wrapS && gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, wrapS);
+      wrapT && gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, wrapT);
 
-      magFilter&&gl.texParameteri(
-        gl.TEXTURE_2D,
-        gl.TEXTURE_MAG_FILTER,
-        magFilter
-      )
+      magFilter &&
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFilter);
 
       if (!minFilter || minFilter > 9729) {
-        gl.generateMipmap(gl.TEXTURE_2D)
+        gl.generateMipmap(gl.TEXTURE_2D);
       }
 
-      minFilter&&gl.texParameteri(
-          gl.TEXTURE_2D,
-          gl.TEXTURE_MIN_FILTER,
-          minFilter
-        )
+      minFilter &&
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, minFilter);
 
-      const u = gl.getUniformLocation(gl.program, key)
-      gl.uniform1i(u, ind)
-    })
+      const u = gl.getUniformLocation(gl.program, key);
+      gl.uniform1i(u, ind);
+    });
   }
   draw(type = this.type) {
-    const { gl, sourceSize } = this
-    gl.drawArrays(gl[type],0,sourceSize);
+    const { gl, sourceSize } = this;
+    gl.drawArrays(gl[type], 0, sourceSize);
   }
 }
